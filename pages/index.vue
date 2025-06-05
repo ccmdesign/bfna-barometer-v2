8292b965efcae2568970694a03f5f4de5a7ef64d
Claudio Mendonca <claudioccm@gmail.com>
2025-06-05 06:51:08 -0700

    layout for index

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,32 @@
 <template>
-  <h1>Homepage</h1>
+  <bar-section>
+    <p class="h3 text-align:center">The site will spotlight trending transatlantic topics and find potential alignment between the United States, Canada, the United Kingdom, and the European Union as well as its 27 member states.</p>
+  </bar-section>
+  
+  <bar-map-section id="map" />
+
+  <bar-section>
+    <div class="switcher">
+      <div class="section-panel">
+        <h2>About</h2>
+        <p>There is no closer geopolitical relationship than the transatlantic alliance. Yet, it is often difficult to know how individual actors are engaging on important issues impacting the larger community. With that challenge in mind, the Bertelsmann Foundation has developed the Transatlantic Barometer, an interactive digital platform that provides up-to-date information on pressing issues shaping the transatlantic relationship.</p>
+        <p>The Transatlantic Barometer’s main fixture is a website that provides users with a synopsis of the policy positions and engagement of 31 key transatlantic actors: the United States, Canada, the United Kingdom, and the European Union as well as its 27 member states. The Barometer provides users with two ways of accessing information. First, users can click on featured topics, generating a carousel of all transatlantic stakeholder positions. Alternatively, users can click on individual actors, revealing a carousel connecting the stakeholder to all featured topics. The Barometer also offers explanatory animations, written analyses, and infographics to highlight the factors driving country-specific policy.</p>
+      </div>
+      <div class="section-panel">
+        <h2>Methodology</h2>
+        <p>The project team regularly identifies pressing topics shaping the transatlantic relationship. The team then engages with subject matter experts on both sides of the Atlantic to determine the best approach to encapsulate each topic. All information is reviewed by a comprehensive list of knowledge partners including diplomats, think tank experts, and academic institutions.</p>
+        
+        <h2>Related Projects</h2>
+        <ul>
+          <li><a href="">Transatlantic Periscope</a></li>
+          <li><a href="">Transatlantic Trends 2022</a></li>
+          <li><a href="">RANGE - Transatlantic Forecasting Platform</a></li>
+          <li><a href="">Sustainable Governance Indicators (SGI)</a></li>
+          <li><a href="">Eupinions</a></li>
+        </ul>
+      </div>
+    </div>
+  </bar-section>
 </template>
 
 <script setup>
