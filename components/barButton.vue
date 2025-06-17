d046686f559eeb1f80e58f151b29240e47a0576f
Claudio Mendonca <claudioccm@gmail.com>
2025-06-17 09:57:11 -0700

    barButton

--- components/barButton.vue
+++ components/barButton.vue
@@ -1,5 +1,5 @@
 <template>
-  <ccm-button>
+  <ccm-button class="bar-button">
     <slot />
   </ccm-button>
 </template>
@@ -9,5 +9,7 @@
 </script>
 
 <style scoped>
-
+  .bar-button {
+    --_button-border-radius: var(--_input-border-radius);
+  }
 </style>
