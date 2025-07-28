0e71546763d846e99618e35f950bc31a60c80e58
Claudio Mendonca <claudioccm@gmail.com>
2025-07-28 09:48:26 -0700

    custom-infographics improved layout

--- components/customInfographic.vue
+++ components/customInfographic.vue
@@ -4,9 +4,15 @@
       <NuxtLink 
         :to="data.customInfographicFile.url"
         target="_blank">
-        <div class="custom_infographic-card | stack">
-          <h3>{{ data.title }}</h3>
-          <p>{{ data.infographicDescription }}</p>
+        <div class="custom_infographic-card">
+          <img :src="data.customInfographicFile.url" :alt="data.title" class="custom_infographic-card__image">
+          <div class="custom_infographic-card__content | cluster">
+            <div class="custom_infographic-card__content-inner | text-align:left" split-right>
+              <h3>{{ data.title }}</h3>
+              <p>{{ data.infographicDescription }}</p>
+            </div>
+            <bar-button visual="primary" size="s" color="accent" :to="data.customInfographicFile.url" target="_blank">Download</bar-button>
+          </div>
         </div>
       </NuxtLink>
     </div>
@@ -25,45 +31,21 @@
 
 <style scoped lang="scss">
 
-.custom_infographic-card__wrapper {
-  width: max-content;
-}
-
-a {text-decoration: none;}
-
 .custom_infographic-card {
-  display: flex;
-  justify-content: center;
-  
   border-radius: var(--border-radius-l);
   border: 1px solid var(--base-color-20-tint);
-  padding-inline: var(--space-m);
-  padding-block: var(--space-s);
   background-color: var(--base-color-05-tint);
   box-shadow: var(--box-shadow-s);
-  text-align: left;
-  aspect-ratio: 1 / 1;
-  --_stack-space: var(--space-3xs);
-
-  &:hover {
-    background-color: var(--base-color);
-    box-shadow: var(--box-shadow-m);
-  }
-}
-
-.custom_infographic-card h3 {
-  font-weight: 700;
-  line-height: 1.3;
-  font-size: var(--size-1);
-  display: -webkit-box;
-  -webkit-line-clamp: 2;
-  line-clamp: 2;
-  -webkit-box-orient: vertical;
   overflow: hidden;
 }
 
-.custom_infographic-card__category {
-  font-size: var(--size--1);
-  color: var(--base-color-50-tint);
+.custom_infographic-card__image {
+  width: 100%;
+  height: 100%;
+  object-fit: contain;
+}
+
+.custom_infographic-card__content {
+  padding: var(--space-m);
 }
 </style>
