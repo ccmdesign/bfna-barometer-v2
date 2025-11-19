3f27fde184ec30ba38706fbcd8e054e1a2bbced8
Douglas Cantao <dcp.pereira@gmail.com>
2025-11-18 16:07:45 -0800

    Updating  clarity script

--- plugins/clarityConsent.client.ts
+++ plugins/clarityConsent.client.ts
@@ -1,22 +1,10 @@
-import { defineNuxtPlugin, useCookieConsent, useHead } from '#imports';
-
-const claritySnippet = `!function(c,l,a,r,i,t,y){function sync(){(new Image).src="https://c.clarity.ms/c.gif"}"complete"==document.readyState?sync():window.addEventListener("load",sync);a[c]("metadata",(function(){a[c]("set","C_IS","0")}),!1,!0);if(a[c].v||a[c].t)return a[c]("event",c,"dup."+i.projectId);a[c].t=!0,(t=l.createElement(r)).async=!0,t.src="https://scripts.clarity.ms/0.8.41/clarity.js",(y=l.getElementsByTagName(r)[0]).parentNode.insertBefore(t,y),a[c]("start",i),a[c].q.unshift(a[c].q.pop()),a[c]("set","C_IS","0")}("clarity",document,window,"script",{"projectId":"sxdxhrq0r7","upload":"https://i.clarity.ms/collect","expire":365,"cookies":["_uetmsclkid","_uetvid"],"track":true,"content":true,"report":"https://report.clarity.ms/eus2-tag","keep":["msclkid"],"dob":2148});`;
-
 export default defineNuxtPlugin(() => {
-  const { state } = useCookieConsent();
-
-  useHead(() => {
-    if (!state.value.statistic) {
-      return {};
-    }
-    return {
-      script: [
-        {
-          id: 'clarity-inline',
-          type: 'text/javascript',
-          children: claritySnippet
-        }
-      ]
-    };
-  });
+  const clarity = function (c, l, a, r, i, t, y) {
+    c[a] = c[a] || function () { (c[a].q = c[a].q || []).push(arguments) };
+    t = l.createElement(r); t.async = 1;
+    t.src = "https://www.clarity.ms/tag/" + i;
+    y = l.getElementsByTagName(r)[0];
+    y.parentNode.insertBefore(t, y);
+  };
+  clarity(window, document, "clarity", "script", "sxdxhrq0r7");
 });
