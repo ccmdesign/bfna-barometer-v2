79418f2c70bd98f35f9cb8115c04dba3964b08b4
Douglas Cantao <dcp.pereira@gmail.com>
2026-03-02 16:40:24 -0800

    Fixing choropleth speech bubble positioning

--- composables/countryCoords2.js
+++ composables/countryCoords2.js
@@ -1,10 +1,10 @@
 export const useCountryCoords2 = () => {
   const countryCoords = {
-    ca: { top: 200.7024, left: 230.68975, height: 18.572, width: 35.313, percentLeft: 0.15, percentTop: 0.23 },
-    us: { top: 330.7024, left: 270.68975, height: 18.572, width: 35.313, percentLeft: 0.18, percentTop: 0.33 },
-    es: { top: 350.7024, left: 800.68975, height: 18.572, width: 35.313, percentLeft: 0.53, percentTop: 0.35 },
-    pt: { top: 350.7024, left: 775.68975, height: 18.572, width: 35.313, percentLeft: 0.515, percentTop: 0.35 },
-    eu: { top: 300, left: 500, height: 50, width: 80 }
+    ca: { top: 0.35, left: 230.68975, height: 18.572, width: 35.313, percentLeft: 0.20, percentTop: 0.23 },
+    us: { top: 0.55, left: 270.68975, height: 18.572, width: 35.313, percentLeft: 0.20, percentTop: 0.33 },
+    es: { top: 0.58, left: 800.68975, height: 18.572, width: 35.313, percentLeft: 0.66, percentTop: 0.35 },
+    pt: { top: 0.58, left: 775.68975, height: 18.572, width: 35.313, percentLeft: 0.64, percentTop: 0.35 },
+    eu: { top: 0.58, left: 500, height: 50, width: 80 }
   }
   
   return {
